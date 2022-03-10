36ffcc827748265497a174989c9b6e37f6c38db1
Ali <aligondal8750@gmail.com>
2022-03-11 00:33:48 +0500

    First Commit

--- app/src/main/java/com/example/practice_03/MainActivity.kt
+++ app/src/main/java/com/example/practice_03/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.practice_03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.practice_03.viewModel.MainViewModel
+import com.example.practice_03.viewModel.MainViewModelFactory
+import com.example.practice_03.databinding.ActivityMainBinding
+import com.example.practice_03.repository.Repository
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    val repository=Repository()
+    val factory=MainViewModelFactory(repository)
+    val viewmodel=ViewModelProvider(this,factory)[MainViewModel::class.java]
+        viewmodel.getPost()
+        viewmodel.myResponse.observe(this,
+        Observer {  Respo->
+            Log.d("Response",Respo.userId.toString())
+            Log.d("Response",Respo.id.toString())
+            Log.d("Response",Respo.title.toString())
+            Log.d("Response",Respo.body.toString())
+        })
+
     }
 }
