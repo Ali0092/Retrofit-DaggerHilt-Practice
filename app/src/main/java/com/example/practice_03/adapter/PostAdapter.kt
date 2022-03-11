99d163a3981bbf5f1e746250bfb529003ab5d638
Ali <aligondal8750@gmail.com>
2022-03-11 11:31:05 +0500

    Second Commit

---
+++ app/src/main/java/com/example/practice_03/adapter/PostAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.practice_03.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.practice_03.databinding.PostitemsBinding
+import com.example.practice_03.model.Post
+
+class PostAdapter():RecyclerView.Adapter<PostAdapter.MyViewHolder>() {
+    private var postL= emptyList<Post>()
+    class MyViewHolder(val binding:PostitemsBinding):RecyclerView.ViewHolder(binding.root) { }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return PostAdapter.MyViewHolder(
+            PostitemsBinding.inflate(
+             LayoutInflater.from(parent.context),
+             parent,
+            false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.binding.apply {
+           postId.text=postL[position].id.toString()
+           postTitle.text=postL[position].title.toString()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return  postL.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+   fun DataChanges(postList:List<Post>){
+        this.postL=postList
+        this.notifyDataSetChanged()
+    }
+}
